b313923a3122bfb1eafe88ce215e9e71733b612f
ClaudRO <claudiorodriguez7778@gmail.com>
2025-06-04 23:22:01 -0400

    detallebd

--- src/config/db/mongoConfig.js
+++ src/config/db/mongoConfig.js
@@ -3,7 +3,7 @@ import { MONGO_URI } from '../config.js';
 
 export const connectMongoDB = async () => {
   try {
-    const mongoUri =  'mongodb://localhost:27017/vimerzionbd';
+    const mongoUri =  'mongodb+srv://matmolinas:ILA572bXVnhjowIS@cluster0.ca16u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(mongoUri);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
